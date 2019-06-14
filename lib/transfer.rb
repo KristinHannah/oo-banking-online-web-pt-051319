35e944598a9c90aed3e4d1020648a828bc696942
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-14 23:53:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       self.receiver.balance += self.amount 
       self.status = "complete"
     else 
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end 
   end 
   
