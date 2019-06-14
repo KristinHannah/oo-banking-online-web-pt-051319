e7bef57321c659037ecb4d24bf9fab3f17b2f1cb
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-14 23:28:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
+    @status = "pending"
+  end 
+  
+  def linked_receiver
+     @linked_receiver = BankAccount.find_by_name(self.receiver)
+  end 
+  
+  def linked_sender
+    @linked_sender = BankAccount.find_by_name(self.sender)
+  end 
+  
+  def valid?
+   # self.linked_sender.valid? && self.linked_receiver.valid?
+  end 
+  
+  def execute_transaction 
+
+  end 
+  
 end
